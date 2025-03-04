84aee5c0870d8eaa0d8b48a53f82d3a2aa254255
Sergey Gilev <sincer89@mail.ru>
2025-03-04 20:35:14 +0500

    create client and server

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 3345;
+
+            Socket socket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String message = "Клиентское сообщение";
+            out.println(message);
+
+            String response = in.readLine();
+            System.out.println("Ответ от сервера: " + response);
+    }
+}
