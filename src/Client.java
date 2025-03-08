5fbc6ce3994cc86eab5ae1a6449c4f81e38e98b3
Sergey Gilev <sincer89@mail.ru>
2025-03-08 16:31:55 +0500

    add try with resource

--- src/Client.java
+++ src/Client.java
@@ -9,14 +9,15 @@ public class Client {
         String host = "localhost";
         int port = 3345;
 
-            Socket socket = new Socket(host, port);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
             String message = "Клиентское сообщение";
             out.println(message);
 
             String response = in.readLine();
             System.out.println("Ответ от сервера: " + response);
+        }
     }
 }
